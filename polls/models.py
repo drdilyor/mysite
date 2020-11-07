14562887caa10332df558d9bfb1b2b397183f831
drdilyor <drdilyor@outlook.com>
2020-11-07 08:45:01 +0500

    Add: models Question, Choice and Vote

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,29 @@
-from django.db import models
+from django.contrib.auth import get_user_model
+from django.db import models as m
 
-# Create your models here.
+User = get_user_model()
+
+
+class Question(m.Model):
+    title = m.CharField(max_length=200)
+    description = m.TextField()
+    author = m.ForeignKey(User, on_delete=m.CASCADE)
+    pub_date = m.DateTimeField()
+    view_count = m.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+class Choice(m.Model):
+    question = m.ForeignKey(Question, on_delete=m.CASCADE)
+    choice_text = m.CharField(max_length=200)
+
+    def __str__(self):
+        return self.choice_text
+
+
+class Vote(m.Model):
+    question = m.ForeignKey(Question, on_delete=m.CASCADE)
+    choice = m.ForeignKey(Choice, on_delete=m.CASCADE)
+    user = m.ForeignKey(User, on_delete=m.CASCADE)
