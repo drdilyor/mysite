09d310c23e53f3a8ff1f3fee709d39e3c7f07025
drdilyor <drdilyor@outlook.com>
2020-11-07 08:42:15 +0500

    Add: app accounts and custom user model

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,6 +28,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'polls',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -116,3 +117,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'accounts.User'
