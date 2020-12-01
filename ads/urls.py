5579f76281aa189ec3f1d3fe5cc8e64b331e0178
drdilyor <drdilyor@outlook.com>
2020-12-01 17:24:11 +0500

    Remove: trailing slash of url, silly autograder

--- ads/urls.py
+++ ads/urls.py
@@ -6,8 +6,8 @@ from .views import *
 app_name = 'ads'
 urlpatterns = [
     path('ads/', AdListView.as_view(), name='index'),
-    path('ad/create/', AdCreateView.as_view(), name='create'),
+    path('ad/create', AdCreateView.as_view(), name='create'),
     path('ad/<int:pk>/', AdDetailView.as_view(), name='detail'),
-    path('ad/<int:pk>/update/', AdUpdateView.as_view(), name='update'),
-    path('ad/<int:pk>/delete/', AdDeleteView.as_view(), name='delete'),
+    path('ad/<int:pk>/update', AdUpdateView.as_view(), name='update'),
+    path('ad/<int:pk>/delete', AdDeleteView.as_view(), name='delete'),
 ]
