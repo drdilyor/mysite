788960e552f4c2ef39238080db35957648426f86
drdilyor <drdilyor@outlook.com>
2020-12-01 17:10:09 +0500

    Change: index now redirects to ads:index, assignment

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home/main.html'), name='index'),
+    path('', RedirectView.as_view(url='/ads/'), name='index'),
     path('', include('ads.urls')),
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
