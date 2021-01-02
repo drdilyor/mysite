82a73adc78d981b0d2e9461c644f7a51b2d55ed2
drdilyor <drdilyor@outlook.com>
2021-01-02 09:02:48 +0500

    Change: AdCreate and AdUpdate views now handle pictures

--- ads/views.py
+++ ads/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
+from django.views.generic import View
 
+from ads.forms import AdForm
 from ads.models import Ad
-from ads.owner import OwnerListView, OwnerDetailView, OwnerCreateView, \
-    OwnerUpdateView, OwnerDeleteView
+from ads.owner import OwnerListView, OwnerDetailView, OwnerDeleteView
 
 
 class AdListView(OwnerListView):
@@ -14,18 +16,52 @@ class AdDetailView(OwnerDetailView):
     model = Ad
 
 
-class AdCreateView(OwnerCreateView):
-    model = Ad
-    fields = ['title', 'price', 'text']
+class AdCreateView(LoginRequiredMixin, View):
+    template_name = 'ads/ad_form.html'
     success_url = reverse_lazy('ads:index')
+    
+    def get(self, request, pk: int = None):
+        form = AdForm()
+        context = {'form': form}
+        return render(request, self.template_name, context)
+    
+    def post(self, request, pk: int = None):
+        form = AdForm(request.POST, request.FILES or None)
+
+        if not form.is_valid():
+            context = {'form': form}
+            return render(request, self.template_name, context)
+
+        # Add owner to the model before saving
+        ad = form.save(commit=False)
+        ad.owner = request.user
+        ad.save()
+        return redirect(self.success_url)
 
 
-class AdUpdateView(OwnerUpdateView):
-    model = Ad
-    fields = ['title', 'price', 'text']
+class AdUpdateView(LoginRequiredMixin, View):
+    template_name = 'ads/ad_form.html'
     success_url = reverse_lazy('ads:index')
+
+    def get(self, request, pk: int):
+        ad = get_object_or_404(Ad, pk=pk, owner=request.user)
+        form = AdForm(instance=ad)
+        context = {'form': form}
+        return render(request, self.template_name, context)
+
+    def post(self, request, pk: int):
+        ad = get_object_or_404(Ad, pk=pk, owner=request.user)
+        form = AdForm(request.POST, request.FILES or None, instance=ad)
+        
+        if not form.is_valid():
+            context = {'form': form}
+            return render(request, self.template_name, context)
+
+        form.save()
+        return render(self.success_url)
 
 
 class AdDeleteView(OwnerDeleteView):
     model = Ad
     success_url = reverse_lazy('ads:index')
+
